e4a21d8fcc0119a0226988dcf06768e62abd556f
BraianAdrian <braian.adrian.gagliardo@gmail.com>
2023-11-09 18:08:09 -0300

    added spw files

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "tic-tac-toe-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon.png",
+  // Agrega otras rutas y archivos de tu aplicación que desees cachear
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response from cache
+      if (response) {
+        return response;
+      }
+
+      // No cache hit - fetch and cache the response from the network
+      return fetch(event.request).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // Clone the response as it needs to be stored in the cache
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
