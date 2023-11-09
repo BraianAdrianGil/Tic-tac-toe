99a0d7b74cb26d73488be03cee8b3cc94d02718f
BraianAdrian <braian.adrian.gagliardo@gmail.com>
2023-11-09 16:32:32 -0300

    added local storage save

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import { turns } from "./constants";
 import { checkWinnerFrom, checkEndGame } from "./utils/board";
 import { WinnerModal } from "./components/WinnerModal";
 import { confettiFunct } from "./utils/confetiAnimation";
-import Square from "./components/Square/Square";
 import Board from "./components/Board";
 import "./App.css";
 import Turn from "./components/Turn";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(turns.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromLocaStorage = window.localStorage.getItem("turn");
+    return turnFromLocaStorage ? JSON.parse(turnFromLocaStorage) : turns.X;
+  });
   //winner = null: no hay ganador , false : hay empate.
   const [winner, setWinner] = useState(null);
 
@@ -21,6 +29,13 @@ function App() {
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
+
+    // Cambiar el turno si no hay un ganador
+    const newTurn = turn === turns.X ? turns.O : turns.X;
+    if (!checkWinnerFrom(newBoard)) setTurn(newTurn);
+    //Guardamos partida en local storage
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", JSON.stringify(newTurn));
     //Revisar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
@@ -28,16 +43,17 @@ function App() {
       setWinner(newWinner);
     } else if (checkEndGame(newBoard)) {
       setWinner(false);
+      window.localStorage.removeItem("board");
+      window.localStorage.removeItem("turn");
     }
-    // Cambiar el turno si no hay un ganador
-    const newTurn = turn === turns.X ? turns.O : turns.X;
-    if (!checkWinnerFrom(newBoard)) setTurn(newTurn);
   };
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(turns.X);
     setWinner(null);
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
   };
 
   return (
